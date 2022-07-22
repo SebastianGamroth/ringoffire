2e18b03690bf359fd802614dbcf226104ee8b064
user7d11 <user7w11@gmail.com>
2022-07-22 11:18:18 +0200

    Show all players

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Fredy', 'Ula', 'Max'];
     public stack: string[] = [];
     public playerCards: string[] = [];
     public currentPlayer: number = 0;
